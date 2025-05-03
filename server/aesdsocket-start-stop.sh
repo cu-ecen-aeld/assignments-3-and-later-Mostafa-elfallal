c4390f2666deaa4fc0711cd8cd9adf07aac0d330
Mostafa <mostafaelfallal90@gmail.com>
2025-05-04 00:24:14 +0300

    adding /usr/bin/ to aesdsocket-start-stop

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -4,26 +4,26 @@ case "$1" in
     start)
         echo "Starting aesdsocket"
         # Start the aesdsocket service as dameon process using -d option using start-stop-daemon
-        start-stop-daemon -S usr/bin/aesdsocket -- -d
+        start-stop-daemon -S /usr/bin/aesdsocket -- -d
         ;;
     stop)
         echo "Stopping aesdsocket"
         # Stop the aesdsocket service
-        start-stop-daemon -K usr/bin/aesdsocket
+        start-stop-daemon -K /usr/bin/aesdsocket
         ;;
     restart)
         echo "Restarting aesdsocket"
         # Restart the aesdsocket service
-        start-stop-daemon -K usr/bin/aesdsocket
+        start-stop-daemon -K /usr/bin/aesdsocket
         sleep 1
-        start-stop-daemon -S usr/bin/aesdsocket -- -d
+        start-stop-daemon -S /usr/bin/aesdsocket -- -d
         ;;
     status)
         # Check the status of the aesdsocket service
-        if pgrep usr/bin/aesdsocketd > /dev/null; then
-            echo "usr/bin/aesdsocket is running"
+        if pgrep /usr/bin/aesdsocketd > /dev/null; then
+            echo "/usr/bin/aesdsocket is running"
         else
-            echo "usr/bin/aesdsocket is not running"
+            echo "/usr/bin/aesdsocket is not running"
         fi
         ;;
     *)
